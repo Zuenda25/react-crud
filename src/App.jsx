405f4b64789077e8051bb104778e8c3429f35737
Zuenda <zuenda25@gmail.com>
2022-05-17 11:01:24 +0700

    Setup router

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
 import React from 'react';
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
-import Add from './pages/add/add';
-import Home from './pages/home/home';
-import Edit from './pages/edit/edit';
+import HomePage from './pages/home/home';
+import AddPage from './pages/add/add';
 import NotFound from './pages/404/not-found';
+import EditPage from './pages/edit/edit';
+import DetailPage from './pages/detail/detail';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="add" element={<Add />} />
-          <Route path="edit/:id" element={<Edit />} />
+          <Route path="/" element={<HomePage />} />
+          <Route path="add" element={<AddPage />} />
+          <Route path="edit/:id" element={<EditPage />} />
+          <Route path="detail/:id" element={<DetailPage />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </header>
