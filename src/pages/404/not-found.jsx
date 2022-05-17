822d9ad9a3ce7d02d4ee4604f4973d9367388eba
Zuenda <zuenda25@gmail.com>
2022-05-17 11:02:17 +0700

    Add not found page

---
+++ src/pages/404/not-found.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function NotFound() {
+  return (
+    <div className="App">
+      <h1>404 - Not Found</h1>
+      <p>
+        <a href="/">Back to Home</a>
+      </p>
+    </div>
+  );
+}
+
+export default NotFound;
